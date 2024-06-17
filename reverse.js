ff8924a95893b4eb6baa54d0cb8aafc7e98c23ca
jon-jh <hiebert456@gmail.com>
2024-06-17 14:11:39 -0600

    debugging practice

--- reverse.js
+++ reverse.js
@@ -1,8 +1,8 @@
-var input = process.argv[2];
+let input = process.argv[2];
 
 if (input)
   console.log(reverse(input));
 
-function reverse(original) {
-  return orignal.split('').reverseList().join('');
-}
+const reverse = function (input) {
+  return input.split('').reverse().join('');
+};
